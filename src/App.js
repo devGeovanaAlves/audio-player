57ac5347411ed7bb4d77d0581f12c7e3ecd744c6
Geovana Alves <geovana.m.alves@hotmail.com>
2023-07-17 19:43:43 -0300

    feat: Adicionado Header em App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import pictureProfile from "./assets/profile.jpg";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="container">
-
+      <Header picture={pictureProfile} name="Daniel" />
     </div>
   );
 }
