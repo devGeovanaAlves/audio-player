1cf3c019793a207dca0a3560382d246a15edb27f
Geovana Alves <geovana.m.alves@hotmail.com>
2023-07-17 19:56:05 -0300

    feat: Adicionado estrutura do component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+import nextIcon from "../../assets/next.svg";
+import pauseIcon from "../../assets/pause.svg";
+import playIcon from "../../assets/play.svg";
+import previousIcon from "../../assets/previous.svg";
+import stopIcon from "../../assets/stop.svg";
+
+function Footer() {
+  return <footer></footer>;
+}
+
+export default Footer;
