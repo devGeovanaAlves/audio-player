633ddb8211835c3d6050a8af263747b4d38378a6
Geovana Alves <geovana.m.alves@hotmail.com>
2023-07-18 00:39:06 -0300

    feat: adicionado componente Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import pictureProfile from "./assets/profile.jpg";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="container">
       <Header picture={pictureProfile} name="Daniel" />
+      <Footer />
     </div>
   );
 }
