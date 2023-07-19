cf2ccc2acf20f5c6552c137528eebe7b2911bb66
Geovana Alves <geovana.m.alves@hotmail.com>
2023-07-18 23:22:28 -0300

    feat: adicionado MusicCard Component e importadoarray de músicas

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,10 +1,16 @@
 import "./style.css";
+import MusicCard from "../MusicCard";
+import { musics } from "../../musics"; //id, title, artist, description, url, cover
 
 function Main() {
   return (
     <main>
-      <h1>Título</h1>
-      <div className="musics-container"></div>
+      <h1>The best play list</h1>
+      <div className="musics-container">
+        {musics.map((music) => (
+          <MusicCard key={music.id} music={music} />
+        ))}
+      </div>
     </main>
   );
 }
