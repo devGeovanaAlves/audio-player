426220811dd7d5f9fc958f1e835cc57c391c0b28
Geovana Alves <geovana.m.alves@hotmail.com>
2023-07-18 22:48:39 -0300

    feat: adicionado elementos ao footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,16 +8,32 @@ import stopIcon from "../../assets/stop.svg";
 function Footer({ authorName, musicName }) {
   return (
     <footer>
-      <div className="author-container">
+      <div className="data-music-container">
         <h1 className="music-name">Lorem ipsum{musicName}</h1>
         <span className="author-name">Dolor sit{authorName}</span>
       </div>
       <div className="controls-container">
-        <img src={stopIcon} alt="Botão de stop" />
-        <img src={previousIcon} alt="Botão de voltar" />
-        <img className="play-pause" src={playIcon} alt="Botão de play" />
-        <img className="play-pause" src={pauseIcon} alt="Botão de pause" />
-        <img src={nextIcon} alt="Botão de próximo" />
+        <div className="buttons-container">
+          <img className="normal-button" src={stopIcon} alt="Botão de stop" />
+          <img
+            className="normal-button"
+            src={previousIcon}
+            alt="Botão de voltar"
+          />
+          <img className="play-pause" src={playIcon} alt="Botão de play" />
+          <img className="play-pause" src={pauseIcon} alt="Botão de pause" />
+          <img
+            className="normal-button"
+            src={nextIcon}
+            alt="Botão de próximo"
+          />
+        </div>
+
+        <div className="progress-bar-container">
+          <span className="time current">0.00</span>
+          <input type="range" />
+          <span className="time">3.34</span>
+        </div>
       </div>
     </footer>
   );
