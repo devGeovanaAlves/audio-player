7161f1940ce16de96082baea60ee074a666483f6
Geovana Alves <geovana.m.alves@hotmail.com>
2023-07-18 22:55:25 -0300

    Adicionado arquivos para Main component

---
+++ src/components/Main/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+function Main() {
+  return (
+    <main>
+      <h1>Título</h1>
+      <div className="musics-container"></div>
+    </main>
+  );
+}
+
+export default Main;
