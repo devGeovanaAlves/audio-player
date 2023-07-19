930c2584d02731352373ad0161c32f817c94a3d5
Geovana Alves <geovana.m.alves@hotmail.com>
2023-07-18 22:56:02 -0300

    feat: importado Main component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import pictureProfile from "./assets/profile.jpg";
 import Header from "./components/Header";
+import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="container">
       <Header picture={pictureProfile} name="Daniel" />
+      <Main />
       <Footer />
     </div>
   );
