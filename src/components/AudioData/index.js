92a68c23d4352a40e9b278a409258af2c49b4808
Geovana Alves <geovana.m.alves@hotmail.com>
2023-07-22 17:14:54 -0300

    feat: adicionado renderização de metadados do elemento audio

--- src/components/AudioData/index.js
+++ src/components/AudioData/index.js
@@ -1,14 +1,18 @@
 import "./style.css";
 
-function AudioData({ currentMusic, audioRef }) {
-  const handleMetadataLoaded = () => {};
+function AudioData({ currentMusic, audioRef, setDuration, progressBarRef }) {
+  const onLoadedMetadata = () => {
+    const timeSeconds = audioRef.current.duration;
+    setDuration(timeSeconds);
+    progressBarRef.current.max = timeSeconds;
+  };
 
   return (
     <div className="data-music-container">
       <audio
         src={currentMusic.url}
         ref={audioRef}
-        handleMetadataLoaded={handleMetadataLoaded}
+        onLoadedMetadata={onLoadedMetadata}
       ></audio>
       <h1 className="music-name">{currentMusic.title}</h1>
       <span className="author-name">{currentMusic.artist}</span>
