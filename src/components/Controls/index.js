5e42c33ae44a592238b4511663b52bc0ab0427f6
Geovana Alves <geovana.m.alves@hotmail.com>
2023-07-22 11:29:02 -0300

    feat: adicionado renderização dinâmica de button play/pause

--- src/components/Controls/index.js
+++ src/components/Controls/index.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import { useState } from "react";
 import nextIcon from "../../assets/next.svg";
 import pauseIcon from "../../assets/pause.svg";
 import playIcon from "../../assets/play.svg";
@@ -6,13 +7,33 @@ import previousIcon from "../../assets/previous.svg";
 import stopIcon from "../../assets/stop.svg";
 
 function Controls() {
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="buttons-container">
-      <img className="normal-button" src={stopIcon} alt="Botão de stop" />
-      <img className="normal-button" src={previousIcon} alt="Botão de voltar" />
-      <img className="play-pause" src={playIcon} alt="Botão de play" />
-      <img className="play-pause" src={pauseIcon} alt="Botão de pause" />
-      <img className="normal-button" src={nextIcon} alt="Botão de próximo" />
+      <button className="normal-button">
+        <img className="normal-button" src={stopIcon} alt="Botão de stop" />
+      </button>
+
+      <button className="normal-button">
+        <img
+          className="normal-button"
+          src={previousIcon}
+          alt="Botão de voltar"
+        />
+      </button>
+
+      <button className="play-pause">
+        {isPlaying ? (
+          <img className="play-pause" src={pauseIcon} alt="Botão de pause" />
+        ) : (
+          <img className="play-pause" src={playIcon} alt="Botão de play" />
+        )}
+      </button>
+
+      <button className="normal-button">
+        <img className="normal-button" src={nextIcon} alt="Botão de próximo" />
+      </button>
     </div>
   );
 }
