b33a2dee1598f0275c029ed6c00a1c06e5f828e8
Geovana Alves <geovana.m.alves@hotmail.com>
2023-07-22 10:19:02 -0300

    refatored: movido conteúdo de controls-container de Footer component para Controls component

--- src/components/Controls/index.js
+++ src/components/Controls/index.js
@@ -1,7 +1,20 @@
 import "./style.css";
+import nextIcon from "../../assets/next.svg";
+import pauseIcon from "../../assets/pause.svg";
+import playIcon from "../../assets/play.svg";
+import previousIcon from "../../assets/previous.svg";
+import stopIcon from "../../assets/stop.svg";
 
 function Controls() {
-  return;
+  return (
+    <div className="buttons-container">
+      <img className="normal-button" src={stopIcon} alt="Botão de stop" />
+      <img className="normal-button" src={previousIcon} alt="Botão de voltar" />
+      <img className="play-pause" src={playIcon} alt="Botão de play" />
+      <img className="play-pause" src={pauseIcon} alt="Botão de pause" />
+      <img className="normal-button" src={nextIcon} alt="Botão de próximo" />
+    </div>
+  );
 }
 
 export default Controls;
