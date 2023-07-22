782e865186278e06ff65949d429425c4ddc44626
Geovana Alves <geovana.m.alves@hotmail.com>
2023-07-22 10:20:45 -0300

    refatored: movido conteúdo de progress-bar-container de Footer component para ProgressBar component

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,7 +1,13 @@
 import "./style.css";
 
 function ProgressBar() {
-  return;
+  return (
+    <div className="progress-bar-container">
+      <span className="time current">0.00</span>
+      <input type="range" />
+      <span className="time">3.34</span>
+    </div>
+  );
 }
 
 export default ProgressBar;
