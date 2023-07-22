757ce4cae432250027aeaf8a2b06e2d709f65cef
Geovana Alves <geovana.m.alves@hotmail.com>
2023-07-22 11:02:30 -0300

    feat: adicionado renderização de audio

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,18 @@
 import "./style.css";
+import { useState } from "react";
+import { musics } from "../../musics"; //id, title, artist, description, url, cover
 import Controls from "../Controls";
 import ProgressBar from "../ProgressBar";
 
-function Footer({ authorName, musicName }) {
+function Footer() {
+  const [currentMusic, setCurrentMusic] = useState(musics[0]);
+
   return (
     <footer>
       <div className="data-music-container">
-        <h1 className="music-name">Lorem ipsum{musicName}</h1>
-        <span className="author-name">Dolor sit{authorName}</span>
+        <audio src={currentMusic.url}></audio>
+        <h1 className="music-name">{currentMusic.title}</h1>
+        <span className="author-name">{currentMusic.artist}</span>
       </div>
       <div className="controls-container">
         <Controls />
@@ -17,4 +22,4 @@ function Footer({ authorName, musicName }) {
   );
 }
 
-export default Footer;
+export default Footer; //AudioPlayer
