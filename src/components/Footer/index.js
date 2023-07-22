5e3db0b502d43a487ebcff13dfa6988c77c4081e
Geovana Alves <geovana.m.alves@hotmail.com>
2023-07-22 10:22:25 -0300

    refatored: adicionados Controls e ProgressBar components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,6 @@
 import "./style.css";
-import nextIcon from "../../assets/next.svg";
-import pauseIcon from "../../assets/pause.svg";
-import playIcon from "../../assets/play.svg";
-import previousIcon from "../../assets/previous.svg";
-import stopIcon from "../../assets/stop.svg";
+import Controls from "../Controls";
+import ProgressBar from "../ProgressBar";
 
 function Footer({ authorName, musicName }) {
   return (
@@ -13,27 +10,8 @@ function Footer({ authorName, musicName }) {
         <span className="author-name">Dolor sit{authorName}</span>
       </div>
       <div className="controls-container">
-        <div className="buttons-container">
-          <img className="normal-button" src={stopIcon} alt="Botão de stop" />
-          <img
-            className="normal-button"
-            src={previousIcon}
-            alt="Botão de voltar"
-          />
-          <img className="play-pause" src={playIcon} alt="Botão de play" />
-          <img className="play-pause" src={pauseIcon} alt="Botão de pause" />
-          <img
-            className="normal-button"
-            src={nextIcon}
-            alt="Botão de próximo"
-          />
-        </div>
-
-        <div className="progress-bar-container">
-          <span className="time current">0.00</span>
-          <input type="range" />
-          <span className="time">3.34</span>
-        </div>
+        <Controls />
+        <ProgressBar />
       </div>
     </footer>
   );
