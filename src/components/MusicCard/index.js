41da70160eca2ba8ca0f08ebc076d7a724c0bf57
Geovana Alves <geovana.m.alves@hotmail.com>
2023-07-21 22:13:38 -0300

    feat: adicionado tag audio

--- src/components/MusicCard/index.js
+++ src/components/MusicCard/index.js
@@ -3,6 +3,7 @@ import "./style.css";
 function MusicCard({ music }) {
   return (
     <a>
+      <audio src={music.url}></audio>
       <img className="music-cover" src={music.cover} alt="" />
       <span className="music-title">{music.title} </span>
       <p className="music-description">{music.description}</p>
